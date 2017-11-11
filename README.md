ee04c6c2e3b195c40f09c23edd7637caa2047c65
Sergey Koba <s.koba@mobidev.biz>
2017-11-11 21:20:54 +0200

    add api doc

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # bark
 
 Root repo for the project
+
+# Local dev
+- `docker-compose build`
+- `docker-compose up -d`
+- `./bin/setup.sh`
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-../users-service/bin/setup.sh
+../users-service/bin/setup.sh  # run bundle
+cd ../users-service/docs && mkdocs build # build api doc
